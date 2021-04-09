d38f4f4c4593b27735d399803eb3b188a2a60bb6
arunshanren <srarun160@gmail.com>
2021-04-09 13:11:36 +0530

    https://github.com/arunshanren/gitPractice.git

---
+++ src/test/java/org/arunsolutions/GoliSoda.java
@@ -0,0 +1,28 @@
+package org.arunsolutions;
+
+public class GoliSoda {
+	
+	public void blueberryFlavour() {
+
+		System.out.println("tastes good");
+	}
+	
+	public void mintFlavour() {
+
+		System.out.println("tastes good");
+	}
+	public void colaFlavour() {
+
+		System.out.println("tastes good");
+	}
+	
+
+	public static void main(String[] args) {
+
+		GoliSoda goli = new GoliSoda();
+		goli.blueberryFlavour();
+		goli.colaFlavour();
+		goli.mintFlavour();
+	}
+
+}
